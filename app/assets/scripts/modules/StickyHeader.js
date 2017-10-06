0ed7e47854d3193ffa994bfe8af1141cde8fb1e2
Jejomar <jejomardorongon@gmail.com>
2017-10-07 00:24:22 +0800

    Added sticky header functionality

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,15 +1,35 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
     constructor() {
+        this.siteHeader = $('.site-header');
         this.welcomeLink = $('#welcome-link');
+        this.headerTriggerElement = $('.row--kv');
         this.addSmoothScrolling();
+        this.createHeaderWaypoint();
+        
     }
 
     addSmoothScrolling() {
         this.welcomeLink.smoothScroll({offset: -75, speed: 500});
     }
+
+    createHeaderWaypoint() {
+        var that = this;
+        new Waypoint({
+            element: this.headerTriggerElement[0],
+            handler: function(direction) {
+                if(direction == 'down') {
+                    that.siteHeader.addClass('site-header--sticky');
+                } else {
+                    that.siteHeader.removeClass('site-header--sticky');
+                }
+            }
+        });
+    }
+
 }
 
 export default StickyHeader;
